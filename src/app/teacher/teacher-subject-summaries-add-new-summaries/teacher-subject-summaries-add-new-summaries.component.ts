2b743aafc1b7e6194f9ccbafb4be834d76a183e6
Hassan-Ali-Mohamed <119622080+Hassan-Ali-Mohamed@users.noreply.github.com>
2024-06-25 01:10:40 +0300

    Add files via upload

---
+++ src/app/teacher/teacher-subject-summaries-add-new-summaries/teacher-subject-summaries-add-new-summaries.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+import {TeacherNavigationBarComponent} from "../teacher-navigation-bar/teacher-navigation-bar.component";
+
+@Component({
+  selector: 'app-teacher-subject-summaries-add-new-summaries',
+  standalone: true,
+  imports: [
+    TeacherNavigationBarComponent
+  ],
+  templateUrl: './teacher-subject-summaries-add-new-summaries.component.html',
+  styleUrl: './teacher-subject-summaries-add-new-summaries.component.css'
+})
+export class TeacherSubjectSummariesAddNewSummariesComponent {
+
+}
